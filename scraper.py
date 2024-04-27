d2ee9228a15c8e813a780ec6db454ae7582f965c
VeIskar <mlewandowski380@gmail.com>
2024-04-28 00:00:05 +0200

    add: updating the data

--- scraper.py
+++ scraper.py
@@ -1,17 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-#import requests_html  
 import time
 import pandas as pd
 
 
 
 url = 'https://www.coingecko.com/'
-response = requests.get(url)
 
 #check amount of pages via scraping the website
 def max_page(url_):
-    response = requests.get(url)
+    response = requests.get(url_)
 
     if response.status_code == 200:
 
@@ -35,41 +33,56 @@ def max_page(url_):
         print("Failed to retrieve the page. Status code:", response.status_code)
 
 
-print(f"max page on website is: {max_page(url)}")
-pages_ = input("how many pages would you like to scrap?: ")
-int_pages = int(pages_)
-if int_pages>max_page(url):
-    print ("Too many pages")
+#input conditions
+
+cond_1 = True
+
+while cond_1 == True:
+    print("max page on website is: 141")
+    pages_ = input("how many pages would you like to scrap?: ")
+    int_pages = int(pages_)
+    if int_pages > 141 or int_pages < 1:
+        print ("Incorrect number of pages add proper number")
+    else:
+         cond_1 == False
+
+
+def main_scrape(url_):
+
+    response = requests.get(url_)
+    if response.status_code == 200:
+        tb_data = []
+        
+        for i in range(1, int_pages+1):
+            parameters = {'page':i}
+            result = requests.get(url, params=parameters)
+            print(f"scraping page: {i}")
+
+        #table.append(pd.read_html(str(soup))[0])
+        if result.status_code == 200:       
+                soup = BeautifulSoup(result.content, 'html.parser')
+                tb_data.append(pd.read_html(str(soup))[0]) #table is the first element
+        else: 
+                    print(f"Failed to retrieve data about crypto currencies from page {i}: ", result.status_code)
+        
+        return tb_data
+        
+    else:
+        print("Failed to retrieve the page. Status code:", response.status_code)
 
 
 
-if response.status_code == 200:
-    
-    tb_data = []
-    
-    for i in range(1,int_pages):
-       parameters = {'page':i}
-       result = requests.get(url, params=parameters) #.text not necessary if we later get .content
-       
-       print(f"scraping page: {i}")
+if __name__ == 'scraper':
+     while True:
+          tb_data = main_scrape(url)
+          main_tb = pd.concat(tb_data)
 
-       #table.append(pd.read_html(str(soup))[0])
-       if result.status_code == 200:        
-            soup = BeautifulSoup(result.content, 'html.parser')
+          # Remove the first and last columns
+          main_tb.drop(main_tb.columns[[0, -1]], axis=1, inplace=True)
 
-            tb_data.append(pd.read_html(str(soup))[0]) #table is the first element
+          print(tb_data)
+          wait = 5 #change to input  so user can say how many seconds/minutes he wants
+          print(f"updating after {wait} seconds")
 
-       else: 
-            print(f"Failed to retrieve data about crypto currencies from page {i}: ", result.status_code)
-    
-else:
-    print("Failed to retrieve the page. Status code:", response.status_code)
-
-
-#merge the tables into one
-main_tb = pd.concat(tb_data)
-
-# Remove the first and last columns
-main_tb.drop(main_tb.columns[[0, -1]], axis=1, inplace=True)
-
-print(tb_data)
+          time.sleep(wait*60)
+     
